9f9b87d9c2a4d870ff001969a74ea91b1579a23a
Tushar Sharma <tusharsharma@virtualemployee.com>
2024-03-20 10:18:37 +0530

    CHange in contact US

--- src/components/ContactUs/Contactus.jsx
+++ src/components/ContactUs/Contactus.jsx
@@ -1,47 +1,49 @@
-import React from 'react'
+import React from 'react';
 import emailjs from '@emailjs/browser';
-
-
-import LeftImage from '../../assets/bg-sharanyadham.jpg'
-import './Contact.css'
+import LeftImage from '../../assets/bg-sharanyadham.jpg';
+import './Contact.css';
 import MyNavbar from '../Navbar/MyNavbar';
+
 const Contact = () => {
   const sendEmail = (e) => {
-    
     e.preventDefault();
 
     emailjs.sendForm('service_yco0nwi', 'template_5ygekdd', e.target, 'QODm_xeVwWxR-AZWI')
       .then((result) => {
-        document.getElementById('contact-form').reset();;
+        document.getElementById('contact-form').reset();
       }, (error) => {
-          console.log(error.text);
+        console.log(error.text);
       });
   };
+
   return (
     <>
-    <MyNavbar/>
-    <div className='contact' >
-      <div className='leftSide' style={{backgroundImage: `url(${LeftImage})`}} ></div>
-      <div className='rightSide'> <h1> हमसे संपर्क करें</h1>
-      
-
-<form id="contact-form"  method="POST" onSubmit={sendEmail}>
-  <label htmlFor="name">Full Name</label>
-  <input name="name" placeholder="Enter full name..." type="text" />
-  <label htmlFor="email">Email</label>
-  <input name="email" placeholder="Enter email..." type="email" />
-  <label htmlFor="message" name="message">Message</label>
-  <textarea
-    rows="6"
-    placeholder="Enter message..."
-    name="message"
-    required
-  ></textarea>
-  <button type="submit"> Send Message</button>
-</form></div>
-    </div>
+     <div className='nature'>
+      <MyNavbar />
+     
+        <div className="container">
+          <h2>Contact Us</h2>
+          <form id='contact-form' onSubmit={sendEmail}>
+            <div className="form-group">
+              <label htmlFor="name">Name:</label>
+              <input type="text" id="name" name="name" required />
+            </div>
+            <div className="form-group">
+              <label htmlFor="email">Email:</label>
+              <input type="email" id="email" name="email" required />
+            </div>
+            <div className="form-group">
+              <label htmlFor="message">Message:</label>
+              <textarea id="message" name="message" rows="4" required></textarea>
+            </div>
+            <div className="form-group">
+              <button type="submit">Submit</button>
+            </div>
+          </form>
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
